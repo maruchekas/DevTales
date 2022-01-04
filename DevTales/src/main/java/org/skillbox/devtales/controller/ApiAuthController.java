2753ed228ca5f962dc34604bdd95c840d55e1578
maruchekas <salken84@gmail.com>
2022-01-04 23:12:24 +0300

    some changes in authService

--- DevTales/src/main/java/org/skillbox/devtales/controller/ApiAuthController.java
+++ DevTales/src/main/java/org/skillbox/devtales/controller/ApiAuthController.java
@@ -1,5 +1,6 @@
 package org.skillbox.devtales.controller;
 
+import lombok.AllArgsConstructor;
 import org.skillbox.devtales.api.request.AuthRequest;
 import org.skillbox.devtales.api.response.AuthResponse;
 import org.skillbox.devtales.api.response.AuthUserResponse;
@@ -18,27 +19,19 @@ import org.springframework.web.bind.annotation.*;
 import java.security.Principal;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/auth")
 public class ApiAuthController {
 
     private final AuthenticationManager authenticationManager;
-    private final UserRepository userRepository;
     private final AuthServiceImpl authServiceImpl;
-
-    public ApiAuthController(AuthenticationManager authenticationManager,
-                             UserRepository userRepository,
-                             AuthServiceImpl authServiceImpl) {
-        this.authenticationManager = authenticationManager;
-        this.userRepository = userRepository;
-        this.authServiceImpl = authServiceImpl;
-    }
 
     @GetMapping("/check")
     public ResponseEntity<AuthResponse> check(Principal principal) {
         if (principal == null) {
             return ResponseEntity.ok(new AuthResponse());
         }
-        return ResponseEntity.ok(getAuthResponse(principal.getName()));
+        return ResponseEntity.ok(authServiceImpl.getAuthResponse(principal.getName()));
     }
 
     @PostMapping("/login")
@@ -48,7 +41,7 @@ public class ApiAuthController {
         SecurityContextHolder.getContext().setAuthentication(auth);
         User user = (User) auth.getPrincipal();
 
-        return ResponseEntity.ok(getAuthResponse(user.getUsername()));
+        return ResponseEntity.ok(authServiceImpl.getAuthResponse(user.getUsername()));
     }
 
     @GetMapping("/logout")
@@ -60,24 +53,6 @@ public class ApiAuthController {
         return ResponseEntity.ok(authResponse);
     }
 
-    private AuthResponse getAuthResponse(String userName) {
-        org.skillbox.devtales.model.User authUser =
-                userRepository.findByEmail(userName)
-                        .orElseThrow(() -> new UsernameNotFoundException("User with email " + userName + " not found"));
 
-        AuthUserResponse authUserResponse = new AuthUserResponse();
-        authUserResponse.setId(authUser.getId());
-        authUserResponse.setEmail(authUser.getEmail());
-        authUserResponse.setName(authUser.getName());
-        authUserResponse.setPhoto(authUser.getPhoto());
-        authUserResponse.setModeration(authUser.getIsModerator() == 1);
-        authUserResponse.setModerationCount(authUser.getPosts().size());
-
-        AuthResponse authResponse = new AuthResponse();
-        authResponse.setResult(true);
-        authResponse.setAuthUserResponse(authUserResponse);
-
-        return authResponse;
-    }
 
 }
--- DevTales/src/main/java/org/skillbox/devtales/service/impl/AuthServiceImpl.java
+++ DevTales/src/main/java/org/skillbox/devtales/service/impl/AuthServiceImpl.java
@@ -1,15 +1,36 @@
 package org.skillbox.devtales.service.impl;
 
+import lombok.AllArgsConstructor;
 import org.skillbox.devtales.api.response.AuthResponse;
+import org.skillbox.devtales.api.response.AuthUserResponse;
+import org.skillbox.devtales.repository.UserRepository;
 import org.skillbox.devtales.service.AuthService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 public class AuthServiceImpl implements AuthService {
 
-    public AuthResponse result() {
+    private final UserRepository userRepository;
+
+    public AuthResponse getAuthResponse(String userName) {
+        org.skillbox.devtales.model.User authUser =
+                userRepository.findByEmail(userName)
+                        .orElseThrow(() -> new UsernameNotFoundException("User with email " + userName + " not found"));
+
+        AuthUserResponse authUserResponse = new AuthUserResponse();
+        authUserResponse.setId(authUser.getId());
+        authUserResponse.setEmail(authUser.getEmail());
+        authUserResponse.setName(authUser.getName());
+        authUserResponse.setPhoto(authUser.getPhoto());
+        authUserResponse.setModeration(authUser.getIsModerator() == 1);
+        authUserResponse.setModerationCount(authUser.getPosts().size());
+
         AuthResponse authResponse = new AuthResponse();
-        authResponse.setResult(false);
+        authResponse.setResult(true);
+        authResponse.setAuthUserResponse(authUserResponse);
+
         return authResponse;
     }
 
